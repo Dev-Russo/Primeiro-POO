ccccc22591f0d542165a8bcc436eb9ffc9601e01
Dev-Russo <murilorusso10@gmail.com>
2020-05-26 10:15:42 -0300

    first commit

---
+++ Personagem.cs
@@ -0,0 +1,16 @@
+namespace C_
+{
+    public class Personagem
+    {
+        public string nome = "Homem-Aranha";
+
+        public int idade = 17;
+
+        public string armadura;
+
+        public string Atacar()
+        {
+            return "o personagem te atacou";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Personagem peter = new Personagem();
+
+            Console.WriteLine(peter.nome);
+            Console.WriteLine(peter.Atacar());
+
+        }
+    }
+}
